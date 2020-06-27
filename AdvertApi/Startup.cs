7c40f3d81c1ce14e74772dc08b2c944d794adf97
s18969 <s18969@pjwstk.edu.pl>
2020-06-27 12:13:24 +0200

    Added automatically generated API documentation using Swagger

--- AdvertApi/Startup.cs
+++ AdvertApi/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
 
 namespace AdvertApi
@@ -28,6 +29,11 @@ namespace AdvertApi
             {
                 options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
             });
+            //1.Dodanie dokumentacji 
+            services.AddSwaggerGen(config =>
+            {
+                config.SwaggerDoc("v1", new OpenApiInfo { Title = "Advert Api", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +45,13 @@ namespace AdvertApi
             }
 
             app.UseHttpsRedirection();
+
+            //2.Dodanie dokumentacji 
+            app.UseSwagger();
+            app.UseSwaggerUI(config =>
+            {
+                config.SwaggerEndpoint("/swagger/v1/swagger.json", "Advert Api");
+            });
 
             app.UseRouting();
 
